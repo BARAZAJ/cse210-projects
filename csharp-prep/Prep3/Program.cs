a6f2135eb198c3ee76e7d9dc4451c398f2fb0617
BARAZAJ <barazajohn52@gmail.com>
2024-04-27 11:08:45 +0300

    prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,44 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+         
+        
+        
+
+        Random randomNumber = new Random();
+        int MagicNumber = randomNumber.Next (1,101);
+
+        
+        int guessnumber = -1;
+
+       while (guessnumber != MagicNumber)
+       {
+        Console.Write("What is your guess");
+        guessnumber = int.Parse(Console.ReadLine());
+        if (MagicNumber > guessnumber)
+        {Console.WriteLine("Guess a higher number");}
+
+        else if (MagicNumber> guessnumber)
+        {Console.WriteLine("Guess a lower number.");}
+
+        else 
+        {
+            Console.WriteLine("Congratulations. You guessed it!");
+        }
+
+
+
+
+
+
+
+
+       }
+
+
+        
+
+
+
     }
 }
