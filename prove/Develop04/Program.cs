78307509d13f79a63b0725e5bf9fe5af043f1cf9
BARAZAJ <barazajohn52@gmail.com>
2024-05-25 16:39:49 +0300

    loop

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,55 +4,62 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Welcome to the Mindfulness App!");
-        Console.WriteLine("Choose an activity:");
-        Console.WriteLine("1. Breathing Activity");
-        Console.WriteLine("2. Reflection Activity");
-        Console.WriteLine("3. Listing Activity");
-        Console.WriteLine("4. Quit");
-
-        Console.Write("Select a choice from the menu (1-4): ");
-        int choice = Convert.ToInt32(Console.ReadLine());
-
-        switch (choice)
+        while (true)
         {
-            case 1:
-                Console.Write("Welcome to the breathing Activity! ");
-                Console.WriteLine(" ");
-                Console.WriteLine("This activity will help you relax by walking through through breathing in and out slowly. Clear your mind and focus on your breathing.");
-                Console.Write("How long, in seconds, would you like your session to be?");
-                int durationBreathing = Convert.ToInt32(Console.ReadLine());
-                BreathingActivity breathingActivity = new BreathingActivity(durationBreathing);
-                breathingActivity.Run();
-                break;
-                
-            case 2:
-                Console.WriteLine("Welcome to the Reflection activity");
-                Console.WriteLine("This activity will help you reflect on time in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life.");
-                Console.Write("How long in seconds would you like your session to be: ");
-                int durationReflection = Convert.ToInt32(Console.ReadLine());
-                ReflectingActivity reflectingActivity = new ReflectingActivity(durationReflection);
-                reflectingActivity.Run();
-                break;
-                
-            case 3:
-                Console.WriteLine("Welcome to the listing activity!");
-                Console.WriteLine("This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.");
-                Console.Write("How long in seconds would you like the activity to be:  ");
-                int durationListing = Convert.ToInt32(Console.ReadLine());
-                ListingActivity listingActivity = new ListingActivity(durationListing);
-                listingActivity.Run();
-                break;
-                
-            case 4:
-                
-                break;
+            Console.Clear();
+            Console.WriteLine("Welcome to the Mindfulness App!");
+            Console.WriteLine("Choose an activity:");
+            Console.WriteLine("1. Breathing Activity");
+            Console.WriteLine("2. Reflection Activity");
+            Console.WriteLine("3. Listing Activity");
+            Console.WriteLine("4. Quit");
 
-            default:
-                Console.WriteLine("Invalid choice. Please try again.");
-                break;
+            Console.Write("Select a choice from the menu (1-4): ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            if (choice == 4)
+            {
+                break; // Exit the loop and terminate the program
+            }
+
+            Console.Write("Enter the duration of the activity in seconds: ");
+            int duration = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    BreathingActivity breathingActivity = new BreathingActivity(duration);
+                    breathingActivity.Run();
+                    break;
+
+                case 2:
+                    ReflectingActivity reflectingActivity = new ReflectingActivity(duration);
+                    reflectingActivity.Run();
+                    break;
+
+                case 3:
+                    ListingActivity listingActivity = new ListingActivity(duration);
+                    listingActivity.Run();
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+            }
+
+            Console.WriteLine("Press any key to return to the main menu...");
+            Console.ReadKey();
         }
 
         Console.WriteLine("Thank you!!");
     }
 }
+
+
+
+
+
+
+
+
+
