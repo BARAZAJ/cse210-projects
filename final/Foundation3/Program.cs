767d91c3067ff1c941b296fd30e043bf9f7a5db5
BARAZAJ <barazajohn52@gmail.com>
2024-06-04 20:22:31 +0300

    new

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -1,9 +1,27 @@
 using System;
 
-class Program
+public class Program
 {
-    static void Main(string[] args)
+    public static void Main()
     {
-        Console.WriteLine("Hello Foundation3 World!");
+        Address address1 = new Address("123 Main St", "Anytown", "CA", "12345");
+        Address address2 = new Address("456 Elm St", "Othertown", "TX", "67890");
+        Address address3 = new Address("789 Oak St", "Sometown", "FL", "10112");
+
+        Lecture lecture = new Lecture("Tech Talk", "A talk on the latest in technology", "2024-07-10", "10:00 AM", address1, "John Doe", 100);
+        Reception reception = new Reception("Company Gala", "Annual company celebration", "2024-07-15", "7:00 PM", address2, "rsvp@company.com");
+        OutdoorGathering outdoorGathering = new OutdoorGathering("Summer Festival", "A fun day in the sun", "2024-08-05", "12:00 PM", address3, "Sunny");
+
+        Event[] events = { lecture, reception, outdoorGathering };
+
+        foreach (Event eventItem in events)
+        {
+            Console.WriteLine(eventItem.GetStandardDetails());
+            Console.WriteLine();
+            Console.WriteLine(eventItem.GetFullDetails());
+            Console.WriteLine();
+            Console.WriteLine(eventItem.GetShortDescription());
+            Console.WriteLine("\n" + new string('-', 40) + "\n");
+        }
     }
 }
